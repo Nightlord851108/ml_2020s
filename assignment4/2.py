7c3285e4f80487ab91748d4fa3fe2125cd8c6c2d
Patrick <nightlord851108@gmail.com>
2020-05-04 13:36:15 +0800

    UPdate

--- assignment4/2.py
+++ assignment4/2.py
@@ -3,25 +3,26 @@ hw4 problem 2
 '''
 import pandas as pd
 from sklearn.decomposition import FactorAnalysis
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 
 HEADERS = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Species']
 
-def FA(dataset, headers, traget_dimension):
+def FA(dataset, traget_dimension):
     converter = FactorAnalysis(n_components = target_dimension, random_state=0)
-    data_value = dataset[headers[-1]]
-    data_factors = dataset[headers[:-1]]
-    fa_data = converter.fit_transform(data_factors.to_numpy())
+    fa_data = converter.fit_transform(dataset.to_numpy())
     reduced_columns = ['nf{}'.format(str(i+1)) for i in range(traget_dimension)]
     result_data = pd.DataFrame(X_FA, columns=columns)
-    result_data[headers[-1]] = data_value
     return result_data
 
 if __name__ == '__main__':
     iris = pd.read_csv('iris.data', names=HEADERS)
     k = 5
+
     accuracies = []
-    from pprint import pprint
-    pprint(iris)
     for i in range(10):
-        train_data, test_data = train_test_split(iris, test_size=0.3)
+        train_data, test_data, train_ans, test_ans = train_test_split(iris[HEADERS[:-1]], iris[HEADERS[-1]], test_size=0.3)
+        fa_train_data = FA(dataset=train_data, headers=HEADERS, traget_dimension=3)
+        fa_test_data = FA(dataset=test_data, headers=HEADERS, traget_dimension=3)
+        knn = KNeighborsClassifier(n_neighbors=k)
+        knn.fit(x_train, y_train)
